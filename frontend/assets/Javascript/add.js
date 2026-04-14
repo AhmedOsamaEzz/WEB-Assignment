ba35a29b0c573f7752abe2ca40ec8fdae4a0d04f
AhmedOsamaEzz <ahmedosamaezz208@gmail.com>
2026-04-14 19:53:28 +0200

    fixed syntax error and minor issue
    admin dashboard now has All books instead of my books
    add.js now compiles

--- frontend/assets/Javascript/add.js
+++ frontend/assets/Javascript/add.js
@@ -1,5 +1,3 @@
-const scriptUrl = document.currentScript.src;
-const APP_ROOT = scriptUrl.split("assets/Javascript/add.js")[0];
 
 // input handle
 const imagePlaceHolder = document.querySelector("#image");
