fcbce39fdd56e729367d64a4e445bd2782a9df4f
Nour-eldin-adel <62nourel@gmail.com>
2026-05-12 13:02:34 +0300

    Implement edit book functionality

--- Midnight_Scholar/books/urls.py
+++ Midnight_Scholar/books/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     # path('add-book/', views.render, name='add_book_page'),
     path('add/', views.add_book, name='add_book'),
     path('delete/<str:isbn>/', views.delete_book, name='delete_book'),
+    path('edit/<str:isbn>/', views.edit_book, name='edit_book'),
 ]
